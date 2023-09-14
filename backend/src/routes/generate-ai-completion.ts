500339ff2da6cf5cc561aca459f8fc9fac1ce235
Alexander Davis <alexander.davis.098@gmail.com>
2023-09-13 22:03:13 -0300

    :recycle:refactor: changing the name of the template variable to prompt

--- backend/src/routes/generate-ai-completion.ts
+++ backend/src/routes/generate-ai-completion.ts
@@ -8,11 +8,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
     app.post('/ai/generate', async (request, reply) => {
         const bodySchema = z.object({
             videoId: z.string(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5)
         })
 
-        const { videoId, template, temperature } = bodySchema.parse(request.body)
+        const { videoId, prompt, temperature } = bodySchema.parse(request.body)
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -23,7 +23,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
         if (!video.transcription)
             return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
